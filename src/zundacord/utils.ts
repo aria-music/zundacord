27797e803c274d78667a19ea136b6d702216796f
ma1750 <mc.ma1750@gmail.com>
2023-05-06 07:13:25 +0000

    skip animated emoji & timestamp

--- src/zundacord/utils.ts
+++ src/zundacord/utils.ts
@@ -2,7 +2,7 @@
 export function getReadableString(str: string): string {
     return str
         .replace(/https?:\/\/[^\s]+/, "") // http(s) url
-        .replace(/<:.+?:.+?>/g, "") // discord emoji
+        .replace(/<.?:.+?(:.+?)?>/g, "") // discord textformats
         .replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, "") // unicode emoji
         .trim()
 }
