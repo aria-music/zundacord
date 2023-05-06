1f501ec957d53a69a2207a1b9d6279139fa135a4
ma1750 <mc.ma1750@gmail.com>
2023-05-06 08:52:39 +0000

    fix Regex and add test

--- src/zundacord/utils.ts
+++ src/zundacord/utils.ts
@@ -2,7 +2,7 @@
 export function getReadableString(str: string): string {
     return str
         .replace(/https?:\/\/[^\s]+/, "") // http(s) url
-        .replace(/<.?:.+?(:.+?)?>/g, "") // discord textformats
+        .replace(/<a?:[^:]+:[0-9]+>/g, "") // discord emoji & Animated emoji
         .replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, "") // unicode emoji
         .trim()
 }
