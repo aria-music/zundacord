e6b0303d0370761189c0a0ab52b0ec83ed1c56a1
ma1750 <43315739+ma1750@users.noreply.github.com>
2023-05-07 17:19:36 +0900

    read URL as `リンク` (#48)

--- src/zundacord/utils.ts
+++ src/zundacord/utils.ts
@@ -1,7 +1,7 @@
 // remove emojis, Discord Emoji escapes from string
 export function getReadableString(str: string): string {
     return str
-        .replace(/https?:\/\/[^\s]+/, "") // http(s) url
+        .replace(/https?:\/\/[^\s]+/, "リンク") // http(s) url
         .replace(/<a?:[^:]+:[0-9]+>/g, "") // discord emoji & Animated emoji
         .replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, "") // unicode emoji
         .trim()
