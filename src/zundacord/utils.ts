5dc437cbe3709cbc6f7352a5cb183f0c6911e371
mnsinri <khj.mkyohei727@gmail.com>
2023-05-11 15:19:14 +0000

    unsupport old mac os

--- src/zundacord/utils.ts
+++ src/zundacord/utils.ts
@@ -1,7 +1,7 @@
 // remove emojis, Discord Emoji escapes from string
 export function getReadableString(str: string): string {
     return str
-        .replace(/\r?\n|\r/g, " ")
+        .replace(/\r?\n/g, " ")
         .replace(/https?:\/\/[^\s]+/g, "リンク") // http(s) url
         .replace(/<a?:[^:]+:[0-9]+>/g, "") // discord emoji & Animated emoji
         .replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, "") // unicode emoji
